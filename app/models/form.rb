5fa6d93f653afa1ac2ce8735bf47abcfadaa492a
David Hill <david.esmale@gmail.com>
2021-01-03 18:04:31 -0600

    add dependent: :destroy to models

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,5 +1,5 @@
 class Form < ApplicationRecord
   belongs_to :user
-  has_many :questions
+  has_many :questions, dependent: :destroy
   accepts_nested_attributes_for :questions, reject_if: :all_blank, allow_destroy: true
 end
